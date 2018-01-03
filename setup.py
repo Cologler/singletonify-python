ac7a9ac8b68723fda0f3cb44d9348e9b2461e409
Cologler <skyoflw@gmail.com>
2018-01-03 18:10:40 +0800

    add setup so people does not need to download pypit.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import os
+from setuptools import setup, find_packages
+
+long_description = ''
+
+if os.path.isfile('__pypit_desc__.rst'):
+    with open('__pypit_desc__.rst') as fp:
+        long_description = fp.read()
+
+long_description = long_description or ''
+
+setup(
+    long_description = long_description,
+    packages = find_packages(),
+    # auto generated:
+    name = 'singletonify',
+    version = '0.1.0.1',
+    description = '',
+    keywords = ['singleton'],
+    author = 'cologler',
+    author_email = 'skyoflw@gmail.com',
+    url = 'https://github.com/Cologler/singletonify-python',
+    license = 'MIT License',
+    classifiers = [],
+    entry_points = {},
+    zip_safe = False,
+    include_package_data = True,
+    setup_requires = [],
+    install_requires = [],
+    tests_require = [],
+)
